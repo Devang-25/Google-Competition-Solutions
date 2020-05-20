b453a7d927d40766a96d8c17f749066396fdd613
Devang Sharma <devshu25@gmail.com>
2020-05-21 02:10:08 +0530

    Incremental House

---
+++ CodeJam/2020/Round 2/Incremental_House.py
@@ -0,0 +1,37 @@
+# Time:  O(log(L + R))
+# Space: O(1)
+
+# Language: Python 2 
+
+def s(a, d, n):
+    return (2*a + (n-1)*d)*n//2
+
+
+def f(a, d, x):
+    r = int((-(2*a-d)+((2*a-d)**2+8*d*x)**0.5)/(2*d))
+    if s(a, d, r) > x:  # adjust float accuracy
+        r -= 1
+    return r
+
+
+def incremental_house_of_pancakes():
+    L, R = map(int, raw_input().strip().split())
+    is_swapped = False
+    if L < R:
+        L, R = R, L
+        is_swapped = True
+    n = f(1, 1, L-R)
+    L -= s(1, 1, n)
+    if L == R:
+        is_swapped = False
+    l = f(n+1, 2, L)
+    r = f(n+2, 2, R)
+    L -= s(n+1, 2, l)
+    R -= s(n+2, 2, r)
+    if is_swapped:
+        L, R = R, L
+    return "{} {} {}".format(n+l+r, L, R)
+
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, incremental_house_of_pancakes())
